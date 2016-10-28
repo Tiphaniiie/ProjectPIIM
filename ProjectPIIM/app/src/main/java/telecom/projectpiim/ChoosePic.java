eae185e5ce694085b3a26a6aa7a2d854fe1b7d70
Idea <Idea>
2016-10-28 12:18:03 +0200

    onclick opens camera

--- ProjectPIIM/app/src/main/java/telecom/projectpiim/ChoosePic.java
+++ ProjectPIIM/app/src/main/java/telecom/projectpiim/ChoosePic.java
@@ -1,5 +1,8 @@
 package telecom.projectpiim;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,14 +13,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ChoosePic extends AppCompatActivity implements View.OnClickListener {
     Button bCamera;
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_pic);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,7 +37,19 @@ public class ChoosePic extends AppCompatActivity implements View.OnClickListener
             }
         });
         bCamera = (Button) findViewById(R.id.bCamera);
-        bCamera.setOnClickListener(this);
+        bCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 
     @Override
